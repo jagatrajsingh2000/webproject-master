898bf87a8d2549cc94f5d7c0d45134f901325901
jagatrajsingh2000 <jagatrajsingh2000@gmail.com>
2023-02-22 12:03:14 +0530

    #

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const partials_path = path.join(__dirname, "../templates/partials");
 app.set('view engine', 'hbs');
 app.set("views", template_path);
 hbs.registerPartials(partials_path);
+// changed for commit demo
 
 // console.log(static_path);
 app.use(express.static(static_path));
